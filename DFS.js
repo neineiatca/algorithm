b02323616b8f8e008d188107d24e813c0a393196
neineiatca <sen.an.ottawa@gmail.com>
2022-10-03 15:19:11 -0400

    exercise

---
+++ DFS.js
@@ -0,0 +1,39 @@
+const input = [
+  {
+    index: 1,
+    value: 1,
+    children: [{ index: 5, index: 6 }],
+  },
+  {
+    index: 2,
+    value: 2,
+    children: [],
+  },
+  {
+    index: 3,
+    value: 3,
+    children: [{ index: 7, index: 1 }],
+  },
+  {
+    index: 4,
+    value: 4,
+    children: [],
+  },
+  {
+    index: 5,
+    value: 5,
+    children: [{ index: 4, index: 2 }],
+  },
+  {
+    index: 6,
+    value: 6,
+    children: [],
+  },
+  {
+    index: 7,
+    value: 7,
+    children: [],
+  },
+];
+
+const buildTree = () => {};
